6e8226077095a50558eb1aca4e0a810c700b8878
Linh1023 <116327425+Linh1023@users.noreply.github.com>
2023-10-04 23:14:11 +0700

    CHANGE ICON ADMIN

--- admin/template/menu_ad.php
+++ admin/template/menu_ad.php
@@ -35,16 +35,29 @@
                 </div>
                 <ul id='ulMenu' class="mt-2 list-unstyled text-black ">
                     <?php
+                    $listIcon=[
+                    'sticky-note',
+                    'book',
+                    'list',
+                    'place-of-worship',
+                    'percent',
+                    'user',
+                    'sticky-note',
+                    'users-cog',
+                    'box',
+                    'home'
+                            ];
                         $n = 0;
                         if($PQ != null){
                             $n = count($PQ);
+                            echo $n;
                         }
                         for($i = $n-1; $i >= 0;$i--){
                     ?>
                         <a href="index.php?id=<?php echo $PQ[$i]["MaChiTiet"]?>">
                             <li class="row LiMenu mx-0 text-center p-2 align-items-center border-secondary border-size-menu " style = "<?php if($id ==  $PQ[$i]["MaChiTiet"]){echo "background:  #6c757d";}?>">
                                 <div class="col-3 px-0 d-none d-lg-flex justify-content-center">
-                                    <i class="fa fa-yin-yang"></i>
+                                    <i class="fa fa-<?php echo $listIcon[$i] ?>"></i>
                                 </div>
                                 <div class="col-lg-9 col-12 px-0">
                                     <p class="text-font-menu m-0"><?php echo $PQ[$i]["TenChiTiet"]?></p>
