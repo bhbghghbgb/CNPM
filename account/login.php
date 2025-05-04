fd7c66c8d752942e3ff70937e0401b36bb67ab95
nguyenthanhhung <nguyenthanhhung.dev@gmail.com>
2025-05-04 14:12:09 +0700

    Thiết kế lại giao diện login register

--- account/login.php
+++ account/login.php
@@ -1,19 +1,23 @@
 <div id="backgroundDN"></div>
 <form method="post" action="./index.php" id="form">
-    <p class="icon-close" onclick=tatdn()>X</p>
+    <p class="icon-close" onclick=tatdn()>×</p>
     <h2>Đăng nhập</h2>
-    Username:
-    <input type="text" name="username" value="" required />
-    Password:
-    <input type="password" name="password" value="" required />
-    <br />
-    <br />
-    <a style='float:right' href="#" onclick=hienformquenmatkhau()>Quên mật khẩu</a>
-    <br />
-    <br />
-    <input type="submit" class="buttonDN" name="dangnhap" value="Đăng Nhập" />
-    <br />
-    <br />
-    <input type="button" class="buttonDN" name="dangky" value="Đăng Ký" onclick=hienthidangky() />
+    <div class="input-group">
+        <label for="username">Tên đăng nhập</label>
+        <input type="text" id="username" name="username" value="" placeholder="Nhập tên đăng nhập" required />
+    </div>
+    <div class="input-group">
+        <label for="password">Mật khẩu</label>
+        <input type="password" id="password" name="password" value="" placeholder="Nhập mật khẩu" required />
+    </div>
+    <div class="forgot-password">
+        <a href="#" onclick=hienformquenmatkhau()>Quên mật khẩu?</a>
+    </div>
+    <div class="form-action">
+        <input type="submit" class="buttonDN" name="dangnhap" value="Đăng Nhập" />
+    </div>
+    <div class="form-footer">
+        <p>Chưa có tài khoản? <a href="#" onclick=hienthidangky()>Đăng ký ngay</a></p>
+    </div>
     <!-- <?php require 'login_submit.php'; ?> -->
 </form>
--- account/register.php
+++ account/register.php
@@ -1,23 +1,35 @@
 <form method="post" action="" id="formdk">
-    <p class="icon-close" onclick=tatdk()>X</p>
-    <h2>Đăng ký</h2>
-    Username:
-    <input type="text" name="username" value="" required />
-    Password:
-    <input type="password" name="password" value="" required />
-    Email:
-    <input type="email" name="email" value="" required />
-    Tên khách hàng:
-    <input type="text" name="TenKhach" value="" required />
-    Địa chỉ:
-    <input type="text" name="DiaChi" value="" required />
-    Số điện thoại:
-    <input type="number" name="SDT" value="" required />
-    <br />
-    <br />
-    <input type="button" class="buttonDN" name="dangnhap" value="Đăng Nhập" onclick="hienthidangnhap()" />
-    <br />
-    <br />
-    <input type="submit" class="buttonDN" name="dangky" value="Đăng Ký" />
+    <p class="icon-close" onclick=tatdk()>×</p>
+    <h2>Đăng ký tài khoản</h2>
+    <div class="input-group">
+        <label for="reg-username">Tên đăng nhập</label>
+        <input type="text" id="reg-username" name="username" placeholder="Nhập tên đăng nhập" required />
+    </div>
+    <div class="input-group">
+        <label for="reg-password">Mật khẩu</label>
+        <input type="password" id="reg-password" name="password" placeholder="Nhập mật khẩu" required />
+    </div>
+    <div class="input-group">
+        <label for="reg-email">Email</label>
+        <input type="email" id="reg-email" name="email" placeholder="Nhập địa chỉ email" required />
+    </div>
+    <div class="input-group">
+        <label for="reg-name">Họ và tên</label>
+        <input type="text" id="reg-name" name="TenKhach" placeholder="Nhập họ và tên" required />
+    </div>
+    <div class="input-group">
+        <label for="reg-address">Địa chỉ</label>
+        <input type="text" id="reg-address" name="DiaChi" placeholder="Nhập địa chỉ" required />
+    </div>
+    <div class="input-group">
+        <label for="reg-phone">Số điện thoại</label>
+        <input type="tel" id="reg-phone" name="SDT" placeholder="Nhập số điện thoại" required />
+    </div>
+    <div class="form-action">
+        <input type="submit" class="buttonDN" name="dangky" value="Đăng Ký" />
+    </div>
+    <div class="form-footer">
+        <p>Đã có tài khoản? <a href="#" onclick="hienthidangnhap()">Đăng nhập ngay</a></p>
+    </div>
     <?php require 'register_submit.php'; ?>
 </form>
